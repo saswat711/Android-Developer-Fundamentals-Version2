543c64a99c67c429373e54858487648a657c54b5
saswat711 <saswatmund711@gmail.com>
2021-05-18 20:11:29 +0530

    4.3 homework

---
+++ 4.3 Menus and pickers/4.3HomeWork/app/src/main/java/com/example/droidcafeinput/DatePickerFragment.java
@@ -0,0 +1,75 @@
+package com.example.droidcafeinput;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DatePickerFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public DatePickerFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param savedInstanceState Parameter 1.
+     * @return A new instance of fragment DatePickerFragment.
+     */
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker.
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+// Create a new instance of DatePickerDialog and return it.
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        OrderActivity activity = (OrderActivity) getActivity();
+        activity.processDatePickerResult(year, month, dayOfMonth);
+    }
+}
